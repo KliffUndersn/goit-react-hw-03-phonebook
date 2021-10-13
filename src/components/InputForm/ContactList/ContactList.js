eef3adc6baaab02285fb72307fd25aaeb472a971
KliffUndersn <83068780+KliffUndersn@users.noreply.github.com>
2021-10-13 14:30:57 +0300

    up

--- src/components/InputForm/ContactList/ContactList.js
+++ src/components/InputForm/ContactList/ContactList.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types"
 
 export const ContactList = ({filteredContacts, deleteContact }) => {
-    return (<ul>{filteredContacts.map(e => <li key={e.id}>{e.name} : {e.number} <button onClick={()=>deleteContact(e.id)}>Delete</button></li>)    }</ul>)
+    return(<ul>{filteredContacts && filteredContacts.map(e => <li key={e.id}>{e.name} : {e.number} <button onClick={()=>deleteContact(e.id)}>Delete</button></li>)    }</ul>)
    
 }
 
--- src/components/InputForm/FilterContacts/FilterContacts.js
+++ src/components/InputForm/FilterContacts/FilterContacts.js
@@ -2,18 +2,21 @@ import React from 'react'
 import PropTypes from "prop-types"
 
 export const FilterContacts = ({filtered}) => {
-   return(
-      
-       <>
-          <h2>Find contacts by name</h2>
-            <input
-            type="text"
-            name="filter"
-            placeholder="start typing"
-            onChange={filtered}
-            />
-            </>)
-}
+  
+  return(
+     
+      <>
+         <h2>Find contacts by name</h2>
+           <input
+           type="text"
+           name="filter"
+           placeholder="start typing"
+           onChange={filtered}
+           />
+           </>)
+
+  }
+
 
 FilterContacts.propTypes={
    filtered:PropTypes.func.isRequired
